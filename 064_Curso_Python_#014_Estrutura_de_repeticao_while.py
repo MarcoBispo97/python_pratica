f286a71875fd7529aeb7f5ef390563c32db829eb
Marco Bispo <marcobispo97@gmail.com>
2024-03-27 20:52:21 -0300

    novos exercícios

---
+++ 064_Curso_Python_#014_Estrutura_de_repeticao_while.py
@@ -0,0 +1,3 @@
+# crie um programa que leia vários npumeros inteiros pelo teclado
+# o programa só vai parar quando o usuário digitar 999
+# no final mostra quantos npumeros foram digitados, quais eles e a soma deles
---
+++ 065_Curso_Python_#014_Estrutura_de_repeticao_while.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia vários números
+# inteiros pelo teclado.
+# no final da execução mostre a média entre todos
+# os valores e qual foi o maior e menor valor lido.
+# O programa deve perguntar ao usuários se ele quer ou não continuar a gigitar valores
