82e8404edd99925e9af8d7aae1ede5bee0ed2fcf
Marco Bispo <marcobispo97@gmail.com>
2024-04-09 17:45:38 -0300

    tuplas concluidoas

--- 076_Curso_Python_#015_Interrompendo_repeticoes_tupla.py
+++ 076_Curso_Python_#015_Interrompendo_repeticoes_tupla.py
@@ -1,3 +1,31 @@
 # Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos
 # preços na sequencia
 # no final, mostre uma listagem de preços organizando os dados de forma tabular
+# Definindo a tupla com os produtos e preços
+produtos_precos = (
+    ("Arroz", 5.99),
+    ("Feijão", 3.49),
+    ("Macarrão", 2.29),
+    ("Azeite de oliva", 12.99),
+    ("Sal", 1.49),
+    ("Açúcar", 3.99),
+    ("Café", 8.49),
+    ("Leite", 2.99),
+    ("Ovos", 4.99),
+    ("Carne", 15.99),
+    ("Frango", 10.99),
+    ("Peixe", 8.99),
+    ("Pão", 3.49),
+    ("Queijo", 6.99),
+    ("Manteiga", 4.49)
+)
+
+# Imprimir cabeçalho da tabela
+print("{:<20} {:<10}".format("Produto", "Preço"))
+
+# Imprimir linha separadora
+print("-" * 30)
+
+# Imprimir cada produto e seu preço
+for produto, preco in produtos_precos:
+    print("{:<20} R$ {:<10.2f}".format(produto, preco))
--- 077_Curso_Python_#015_Interrompendo_repeticoes_tupla.py
+++ 077_Curso_Python_#015_Interrompendo_repeticoes_tupla.py
@@ -1,4 +1,46 @@
-# Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos
-# preços na sequencia
+# crie um programa que tenha uma tupla com várias palavras
+# Não usar acento depois disso voce mostrará para cada palavra
+# quais são suas vogais
 
-# no final, mostre uma listagem de preços organizando os dados de forma tabular
+palavras_sem_acento = (
+    "banana",
+    "cachorro",
+    "gato",
+    "carro",
+    "casa",
+    "futebol",
+    "escola",
+    "janela",
+    "computador",
+    "livro",
+    "cidade",
+    "menino",
+    "menina",
+    "frio",
+    "verao",
+    "chocolate",
+    "amigo",
+    "amor",
+    "trabalho",
+    "musica"
+)
+
+
+for palavra in palavras_sem_acento:
+    a = 0
+    e = 0
+    i = 0
+    o = 0
+    u = 0
+    for letra in palavra:
+        if letra == "a":
+            a += 1
+        elif letra == "e":
+            e += 1
+        elif letra == "i":
+            i += 1
+        elif letra == "o":
+            o += 1
+        elif letra == "u":
+            u += 1
+    print(f"A palavra {palavra}, tem: \n {a} a, {e} e, {i} i, {o} o, {u} u")
