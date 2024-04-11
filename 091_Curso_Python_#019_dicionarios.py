77b4bf3718a386fcba2421b0341e4a0fcef6d761
Marco Bispo <marcobispo97@gmail.com>
2024-04-11 12:13:36 -0300

    enunciados dicionário

---
+++ 091_Curso_Python_#019_dicionarios.py
@@ -0,0 +1,4 @@
+# Crie um programa onde 4 jogadores joguem um dado e
+# tenha resultados aleatórios
+# No final coloque esse dicionário em ordem
+# sabendp qie p vencedor tirou o maior número no dado
---
+++ 093_Curso_Python_#019_dicionarios.py
@@ -0,0 +1,7 @@
+# Crie um programa que gerencie o aproveitamento de um
+# jogador de futebol. O programa vai ler o
+# Nome do jogador e quantas partidas ele jogou
+# Depois vai ler a quantidade de gols
+# feitos em cada partida
+# no final, tudo isso será guardado em um dicionário,
+# incluindo o total de gols feitos durante o campeonatio
