bd134cf3299a4d6a370eac46af290c8b5f03a5b2
Marco Bispo <marcobispo97@gmail.com>
2024-04-27 13:58:02 -0300

    exercicio 95

---
+++ teste.py
@@ -0,0 +1,9 @@
+# Lista de dicionários
+lista_de_dicionarios = [
+    {'chave1': 'valor1', 'chave2': 'valor2'},
+    {'chave1': 'valor3', 'chave2': 'valor4'}
+]
+
+# Acessando o valor da chave 'chave1' no primeiro dicionário da lista
+valor = lista_de_dicionarios[0]['chave1']
+print(valor)  # Isso imprimirá 'valor1'
