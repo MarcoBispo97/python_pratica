503dc54a54291f4d446dc3f1acd93ead52621db0
Marco Bispo <marcobispo97@gmail.com>
2024-05-03 19:02:13 -0300

    exercicios de funções resolvidos

--- 104_Curso_Python_#021_Funcoes.py
+++ 104_Curso_Python_#021_Funcoes.py
@@ -4,3 +4,17 @@
 # só que fazendo a validação feita
 # para aceitar apenas int
 # Ex: n = leia("Digite um n")
+
+def leiaint():
+    while True:
+        num = input("Digite um numero: ")
+        if num.isnumeric():
+            numint = int(num)
+            print(f'você digitou o número {numint}')
+            break
+        else:
+            print("\033[0;31mErro digite um número!\033[m")
+    return numint
+
+
+leiaint()
--- 105_Curso_Python_#021_Funcoes.py
+++ 105_Curso_Python_#021_Funcoes.py
@@ -1,4 +1,4 @@
-# Faça um programa que tenha uma 
+# Faça um programa que tenha uma
 # função notas que pode receber várias notas
 # de um aluno e vai retornar um dicionário
 # com a seguintes informações
@@ -9,3 +9,28 @@
 
 # adcicione tambem as docstrings
 # na função
+
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas e a situações de vários alunos (aceita várias notas)
+    :param sit: valor opicional, indicando se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre a situação da turma
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = round(sum(n) / len(n), 2)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+resp = notas(3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 9, 8, 9, sit=True)
+print(resp)
